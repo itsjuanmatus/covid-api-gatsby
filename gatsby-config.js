2e314d3f4f72d0ca3f45616a7d70393d57fc8143
itsjuanmatus <juanmatusg16@gmail.com>
2021-10-28 21:58:09 -0600

    Deleted siteurl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,5 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "spa-covid-api-gatsby.netlify.app",
     title: "covid api",
   },
   plugins: ["gatsby-plugin-netlify-cms", `gatsby-plugin-postcss`, 'gatsby-plugin-typescript'],
